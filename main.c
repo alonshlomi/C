efdc044e235a78fe2a4a260265319c20c9c5f6b1
Alon <alonp13@gmail.com>
2019-11-07 19:10:44 +0000

    Final version

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include "myMath.h"
 
-
 int main()
-{
-  printf("add(1,3)= %f \n",add(1,3) );
-  printf("sub(5,3)= %f \n",sub(5,3) );
-  printf("mul(3,3)= %lf \n",mul(3,3) );
-  printf("div(2,3)= %lf \n",div(2,3) );
+{	
+	double x;
+	printf("Please insert a real number: ");
+	scanf("%lf",&x);
+	printf("\n");
 
-  printf("pow(2,3)= %lf \n",pow(2,3) );
-  printf("exp(2)= %lf \n",exp(2) );
+	float fx_1 = sub(add(exp(x),pow(x,3)),2);
+	float fx_2 = add(mul(x,3),mul(pow(x,2),2));
+	float fx_3 = sub(div(mul(pow(x,3),4),5),mul(x,2));
+	
+	printf("The value of f(x) = e^x+x^3-2 at the point %0.4lf is: %0.4f \n",x,fx_1);
+	printf("The value of f(x) = 3x+2x^2 at the point %0.4lf is: %0.4f\n",x,fx_2);	
+	printf("The value of f(x) = (4x^3)/5-2x at the point %0.4lf is: %0.4f\n",x,fx_3);	
+
+
+
 
   return 0;
 
--- power.c
+++ power.c
@@ -1,21 +1,22 @@
 #include "myMath.h"
 
-double Result =1;
-double e = 2.71828;
 double exp(int x)
 {
-  for(int i=0; i<x; i++)
-  {
-        Result *= e;
-  }
-  return Result;
+  	double res =1;
+  	double e = 2.7183;
+  	for(int i=0; i<x; i++)
+  	{
+  	      res = res * e;
+  	}
+  return res;
 }
 
 double pow(double x, int y)
 {
-  for(int i=0; i<y; i++)
-    {
-      Result*=x;
-    }
-    return Result ;
-  }
+	double res= 1;
+  	for(int i=0; i<y; i++)
+    	{
+      	res = res * x;
+    	}
+    	return res ;
+}
